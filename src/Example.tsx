a788c114fd30023827f375c0d58482d41144ffe2
ahw <andrewhallagan@gmail.com>
2025-12-06 17:40:25 -0500

    Working example

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,43 +1,153 @@
-import { useQuery } from "@tanstack/react-query";
-import { useMemo, useState } from "react";
-import { DirectorySearchClient } from "./services/DirectorySearchClient";
+import { useQuery, type QueryFunction } from "@tanstack/react-query";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  DirectorySearchClient,
+  type EventCallbackData,
+} from "./services/DirectorySearchClient";
 
 export function Example() {
   const [userQuery, setUserQuery] = useState("");
   const searchClient = useMemo(() => new DirectorySearchClient(), []);
+  const search: QueryFunction<string[], string[]> = useCallback(
+    ({ queryKey }) => {
+      return searchClient.search(queryKey?.[0]);
+    },
+    [searchClient]
+  );
+
+  const [searchClientEvents, setSearchClientEvents] = useState<string[]>([]);
+
+  useEffect(() => {
+    const eventHandler = (data: EventCallbackData) => {
+      setSearchClientEvents((events) => [
+        ...events,
+        data.eventName + ": " + data.data.join(", "),
+      ]);
+    };
+
+    searchClient.addListener("query", eventHandler);
+    searchClient.addListener("searchCompleted", eventHandler);
+    searchClient.addListener("searchFailed", eventHandler);
+
+    return () => {
+      searchClient.removeListener("query", eventHandler);
+      searchClient.removeListener("searchCompleted", eventHandler);
+      searchClient.removeListener("searchFailed", eventHandler);
+    };
+  }, [searchClient]);
+
   const {
-    isPending,
+    data,
+    status,
+    error,
+    failureCount,
+    failureReason,
+    fetchStatus,
+    isEnabled,
+    isError,
+    isFetched,
+    isFetchedAfterMount,
     isFetching,
     isLoading,
+    isLoadingError,
+    isPaused,
+    isPending,
+    isPlaceholderData,
+    isRefetchError,
     isRefetching,
     isStale,
     isSuccess,
-    isPaused,
-    isError,
-    isFetched,
-    error,
-    data,
   } = useQuery({
-    enabled: ({ queryKey }) => queryKey?.[0]?.length > 0,
     queryKey: [userQuery],
-    queryFn: async ({ queryKey }) => {
-      return searchClient.search(queryKey?.[0]);
-    },
+    enabled: ({ queryKey }) => queryKey?.[0]?.length > 0,
+    queryFn: search,
   });
 
+  const statesToDisplay = {
+    status,
+    fetchStatus,
+    error,
+    failureCount,
+    failureReason,
+    isEnabled,
+    isError,
+    isFetched,
+    isFetchedAfterMount,
+    isFetching,
+    isLoading,
+    isLoadingError,
+    isPaused,
+    isPending,
+    isPlaceholderData,
+    isRefetchError,
+    isRefetching,
+    isStale,
+    isSuccess,
+  };
+
+  const stateItems = useMemo(() => {
+    return Array.from(Object.entries(statesToDisplay));
+  }, [statesToDisplay]);
+
   return (
-    <div>
-      <input
-        type="text"
-        value={userQuery}
-        onChange={(e) => setUserQuery(e.target.value)}
-      />
-      <h1>results</h1>
-      <ul style={{ height: 200, overflow: "auto", border: "1px solid black" }}>
-        {data?.map((result) => (
-          <li key={result}>{result}</li>
-        ))}
-      </ul>
+    <div style={{ display: "flex", gap: "2rem" }}>
+      <div>
+        <h1>Search</h1>
+        <input
+          type="text"
+          style={{
+            width: "100%",
+            marginBottom: "1rem",
+            border: "1px solid black",
+            fontFamily: "monospace",
+          }}
+          value={userQuery}
+          onChange={(e) => setUserQuery(e.target.value)}
+        />
+        <h1>Results</h1>
+        <div
+          style={{ height: 200, overflow: "auto", border: "1px dashed black" }}
+        >
+          {data?.map((result) => (
+            <code style={{ display: "block" }} key={result}>
+              {result}
+            </code>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h1>React Query States</h1>
+        <table style={{ border: "1px dashed black" }}>
+          {stateItems.map(([key, value]) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td
+                style={{
+                  backgroundColor:
+                    typeof value === "boolean"
+                      ? value
+                        ? "lightgreen"
+                        : "lightcoral"
+                      : "transparent",
+                }}
+              >
+                {String(value)}
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+
+      <div>
+        <h1>DirectorySearchClient events</h1>
+        <div>
+          {searchClientEvents.map((event, index) => (
+            <div key={index} style={{ fontFamily: "monospace" }}>
+              {event}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/services/DirectorySearchClient.ts
+++ src/services/DirectorySearchClient.ts
@@ -1,9 +1,13 @@
 import EventEmitter from "eventemitter3";
 
+export interface EventCallbackData {
+  eventName: string;
+  data: string[];
+}
 interface Events {
-  query: (query: string) => void;
-  searchCompleted: (results: string[]) => void;
-  searchFailed: (error: Error) => void;
+  query: (data: EventCallbackData) => void;
+  searchCompleted: (data: EventCallbackData) => void;
+  searchFailed: (data: EventCallbackData) => void;
 }
 
 export class DirectorySearchClient extends EventEmitter<Events> {
@@ -13,17 +17,49 @@ export class DirectorySearchClient extends EventEmitter<Events> {
     this.entries = [
       "apple",
       "banana",
+      "blackberry",
+      "blueberry",
+      "cantaloupe",
       "cherry",
+      "currant",
+      "date plum",
       "date",
+      "dragonfruit",
       "elderberry",
+      "elderflower",
+      "feijoa",
       "fig",
       "grape",
       "honeydew",
+      "jackfruit",
+      "kiwi",
+      "lemon",
+      "lychee",
+      "mango",
+      "mulberry",
+      "nectarine",
+      "orange",
+      "papaya",
+      "persimmon",
+      "pineapple",
+      "pomegranate",
+      "quince",
+      "raspberry",
+      "starfruit",
+      "strawberry",
+      "tamarind",
+      "tangerine",
+      "ugli fruit",
+      "voavanga",
+      "watermelon",
+      "xigua",
+      "yellow passion fruit",
+      "zucchini",
     ];
   }
 
   async search(query: string): Promise<string[]> {
-    this.emit("query", query);
+    this.emit("query", { eventName: "query", data: [query] });
     try {
       // Simulate an asynchronous search operation
       const results = await new Promise<string[]>((resolve) => {
@@ -32,11 +68,18 @@ export class DirectorySearchClient extends EventEmitter<Events> {
         }, 500);
       });
 
-      this.emit("searchCompleted", results);
+      this.emit("searchCompleted", {
+        eventName: "searchCompleted",
+        data: [query, `${results.length} results`],
+      });
       return results;
     } catch (error) {
-      this.emit("searchFailed", error as Error);
+      this.emit("searchFailed", { eventName: "searchFailed", data: [] });
       throw error;
     }
+  }
+
+  async listAll(): Promise<string[]> {
+    return this.entries;
   }
 }

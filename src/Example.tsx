17afd425210fecab23080772dc69b26877c4dcec
ahw <andrewhallagan@gmail.com>
2025-12-07 15:19:34 -0500

    SearchClientEventBlotter

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,12 +1,9 @@
 import { useQuery, type QueryFunction } from "@tanstack/react-query";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { AMERICAN_CITIES } from "./data/AMERICAN_CITIES";
 import { ExampleControls } from "./ExampleControls";
-import {
-  TextSearchClient,
-  type EventCallbackData,
-} from "./services/TextSearchClient";
-import { formatNumber } from "./utils/format";
+import { SearchClientEventBlotter } from "./SearchClientEventBlotter";
+import { TextSearchClient } from "./services/TextSearchClient";
 
 export interface ExampleState {
   delay?: number;
@@ -34,34 +31,6 @@ export function Example() {
     },
     [searchClient]
   );
-
-  const t0 = useRef<number | undefined>(undefined);
-  const [searchClientEvents, setSearchClientEvents] = useState<
-    { time: number; data: string }[]
-  >([]);
-
-  useEffect(() => {
-    const eventHandler = (data: EventCallbackData) => {
-      t0.current ??= Date.now();
-      setSearchClientEvents((events) => [
-        ...events,
-        {
-          time: Date.now(),
-          data: data.eventName + ": " + data.data.join(", "),
-        },
-      ]);
-    };
-
-    searchClient.addListener("query", eventHandler);
-    searchClient.addListener("searchCompleted", eventHandler);
-    searchClient.addListener("searchFailed", eventHandler);
-
-    return () => {
-      searchClient.removeListener("query", eventHandler);
-      searchClient.removeListener("searchCompleted", eventHandler);
-      searchClient.removeListener("searchFailed", eventHandler);
-    };
-  }, [searchClient]);
 
   const {
     data,
@@ -175,22 +144,7 @@ export function Example() {
           </table>
         </div>
 
-        <div>
-          <h1>searchClient events</h1>
-          <table className="box box_border">
-            {searchClientEvents.map((event, index) => (
-              <tr key={index} style={{ fontFamily: "monospace" }}>
-                <td className="px-1 text-gray-500">
-                  {formatNumber((event.time - (t0.current ?? 0)) / 1000, {
-                    maximumFractionDigits: 3,
-                    minimumFractionDigits: 3,
-                  })}
-                </td>
-                <td className="px-1">{event.data}</td>
-              </tr>
-            ))}
-          </table>
-        </div>
+        <SearchClientEventBlotter searchClient={searchClient} />
       </div>
     </div>
   );
---
+++ src/SearchClientEventBlotter.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useRef, useState } from "react";
+import type {
+  EventCallbackData,
+  TextSearchClient,
+} from "./services/TextSearchClient";
+import { formatNumber } from "./utils/format";
+
+export function SearchClientEventBlotter({
+  searchClient,
+}: {
+  searchClient: TextSearchClient;
+}) {
+  const t0 = useRef<number | undefined>(undefined);
+  const [searchClientEvents, setSearchClientEvents] = useState<
+    { time: number; data: string }[]
+  >([]);
+
+  useEffect(() => {
+    const eventHandler = (data: EventCallbackData) => {
+      t0.current ??= Date.now();
+      setSearchClientEvents((events) => [
+        ...events,
+        {
+          time: Date.now(),
+          data: data.eventName + ": " + data.data.join(", "),
+        },
+      ]);
+    };
+
+    searchClient.addListener("query", eventHandler);
+    searchClient.addListener("searchCompleted", eventHandler);
+    searchClient.addListener("searchFailed", eventHandler);
+
+    return () => {
+      searchClient.removeListener("query", eventHandler);
+      searchClient.removeListener("searchCompleted", eventHandler);
+      searchClient.removeListener("searchFailed", eventHandler);
+    };
+  }, [searchClient]);
+
+  return (
+    <div>
+      <h1>searchClient events</h1>
+      <table className="box box_border">
+        {searchClientEvents.map((event, index) => (
+          <tr key={index} style={{ fontFamily: "monospace" }}>
+            <td className="px-1 text-gray-500">
+              {formatNumber((event.time - (t0.current ?? 0)) / 1000, {
+                maximumFractionDigits: 3,
+                minimumFractionDigits: 3,
+              })}
+            </td>
+            <td className="px-1">{event.data}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
